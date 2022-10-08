9111031a7cf7beb1d74526aae3a0bfa206a8d01a
Muhammad Bilal <bilal.dilbar54@gmail.com>
2022-10-08 19:34:26 -0400

    implemented get request

--- images.js
+++ images.js
@@ -3,9 +3,9 @@ var Port = 5000;
 var Host = '127.0.0.1';
 
 var restify = require('restify')
-
+ // Get a persistence engine for the images
 , imagesSave = require('save')('images')
-
+  // Create the restify server
 , server = restify.createServer({ name: Server_Name })
 
 server.listen(Port, Host, function()
@@ -17,5 +17,22 @@ server.listen(Port, Host, function()
 })
 
 server
+    // Allow the use of POST
     .use(restify.fullResponse())
+    // Maps req.body to req.params so there is no switching between them
     .use(restify.bodyParser())
+
+// Get all images in the system
+server.get('/images', function (req, res, next)
+{
+    console.log('')
+    // Log request information
+    console.log('> images GET ALL: received request')
+    // Find every entity within the given collection
+    imagesSave.find({}, function (error, images)
+    {
+        res.send(images)
+    })
+    //Log response information
+    console.log('< images GET ALL: sending response')
+})
