2fd30e04ddf36b39b7a5c5cf25538262fd47dd62
Muhammad Bilal <bilal.dilbar54@gmail.com>
2022-10-08 19:23:20 -0400

    created package.json

--- images.js
+++ images.js
@@ -2,11 +2,20 @@ var Server_Name = 'imagesdata-API'
 var Port = 5000;
 var Host = '127.0.0.1';
 
+var restify = require('restify')
 
-server.listen(Port, Host, function())
+, imagesSave = require('save')('images')
+
+, server = restify.createServer({ name: Server_Name })
+
+server.listen(Port, Host, function()
 {
     console.log('Server is listening at %s', server.url)
-    console.log('Endpoints')
+    console.log('Endpoints:')
     console.log(server.url + '/images')
     console.log('method: GET, POST, DELETE')
-}
+})
+
+server
+    .use(restify.fullResponse())
+    .use(restify.bodyParser())
