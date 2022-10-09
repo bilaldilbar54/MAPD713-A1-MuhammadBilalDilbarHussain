fa2d22971fdbd61a13457efd606583820e327cc3
Muhammad Bilal <bilal.dilbar54@gmail.com>
2022-10-08 20:21:23 -0400

    fixed bugs

--- images.js
+++ images.js
@@ -5,14 +5,16 @@ var ReqPost = 0;
 var ReqGet = 0;
 
 var restify = require('restify')
- // Get a persistence engine for the images
+// Get a persistence engine for the images
 , imagesSave = require('save')('images')
-  // Create the restify server
+// Create the restify server
 , server = restify.createServer({ name: Server_Name })
 
 server.listen(Port, Host, function()
 {
+    console.log('')
     console.log('Server is listening at %s', server.url)
+    console.log('')
     console.log('Endpoints:')
     console.log(server.url + '/images')
     console.log('method: GET, POST, DELETE')
@@ -27,6 +29,8 @@ server
 // Getting all images in the system
 server.get('/images', function (req, res, next)
 {
+    //Incrementing the GET request counter
+    ReqGet++;
     console.log('')
     // Log request information
     console.log('> images GET ALL: received request')
@@ -37,6 +41,7 @@ server.get('/images', function (req, res, next)
     })
     // Log response information
     console.log('< images GET ALL: sending response')
+    console.log('')
     // Request Counters (GET & POST)
     console.log('Processed Request Count-->')
     console.log('GET: ' + ReqGet)
@@ -46,7 +51,10 @@ server.get('/images', function (req, res, next)
 // Creating a new image
 server.post('/images', function (req, res, next)
 {
+    //Incrementing the POST request counter
+    ReqPost++;
     console.log('')
+    // Log request information
     console.log('> images POST: received request')
     // Make sure name is defined
     if (req.params.imageID === undefined ) 
@@ -54,21 +62,25 @@ server.post('/images', function (req, res, next)
         // If there are any errors, pass them to next in the correct format
         return next(new restify.InvalidArgumentError('imageID must be supplied'))
     }
+
     if (req.params.name === undefined ) 
     {
         // If there are any errors, pass them to next in the correct format
         return next(new restify.InvalidArgumentError('name must be supplied'))
     }
+
     if (req.params.url === undefined ) 
     {
         // If there are any errors, pass them to next in the correct format
         return next(new restify.InvalidArgumentError('url must be supplied'))
     }
+
     if (req.params.size === undefined ) 
     {
         // If there are any errors, pass them to next in the correct format
         return next(new restify.InvalidArgumentError('size must be supplied'))
     }
+
     var newImage = 
     {
         imageID: req.params.imageID,
@@ -76,29 +88,35 @@ server.post('/images', function (req, res, next)
         url: req.params.url,
         size: req.params.size
     }
-    // Creating the image using the persistence engine
+    // Create the image using the persistence engine
     imagesSave.create( newImage, function (error, image)
     {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
         res.send(201, image)
     })
+    // Log response information
     console.log('< images POST: saving response')
+    console.log('')
     // Request Counters (GET & POST)
     console.log('Processed Request Count-->')
     console.log('GET: ' + ReqGet)
     console.log('POST: ' + ReqPost)
 })
 
+// Delete all images
 server.del('/images', function (req, res, next)
 {
     console.log('')
+    // Log request information
     console.log('> images DELETE ALL: received request')
     imagesSave.deleteMany({}, function (error, image)
     {
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
         res.send(200, 'Images Deleted')
     })
+    // Log response information
     console.log('< images DELETE ALL: executed')
+    console.log('')
     // Request Counters (GET & POST)
     console.log('Processed Request Count-->')
     console.log('GET: ' + ReqGet)
